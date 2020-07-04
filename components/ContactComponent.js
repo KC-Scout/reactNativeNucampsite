dfca821b0e8b147cc833cd73237edba042ae1156
Connor Foster <foster.clee@gmail.com>
2020-07-04 10:09:32 -0500

    Added in about and contact to drawer navigation

---
+++ components/ContactComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    };
+
+    render() {
+        return (
+            <ScrollView>
+
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact; 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,8 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -40,13 +42,49 @@ const HomeNavigator = createStackNavigator(
             }
         }
     }
+);
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
 );    
 
 
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: {screen: DirectoryNavigator}
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator }
     },
     {
         drawerBackgroundColor: '#CEC8FF'
