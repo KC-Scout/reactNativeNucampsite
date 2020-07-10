670bef50a847133ce534f97be72e8142a53fc3d5
Connor Foster <foster.clee@gmail.com>
2020-07-09 20:23:41 -0500

    Reservation form added to reservation compoent

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,6 +7,7 @@ import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
@@ -117,6 +118,28 @@ const ContactNavigator = createStackNavigator(
     }
 );    
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);    
 
 const CustomDrawerContentComponent = props => (
     <ScrollView>
@@ -159,6 +182,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({ tintColor }) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
