f1d32c7730a4acd5bcaa2e8da9ab83d26421e168
Connor Foster <foster.clee@gmail.com>
2020-07-21 17:03:22 -0500

    Local notifications added to reservation component

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Modal, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
     constructor(props) {
@@ -35,7 +37,10 @@ class Reservation extends Component {
                     },
                     {
                         text: 'OK',
-                        onPress: () => { this.resetForm() }
+                        onPress: () => {
+                            this.presentLocalNotification(this.state.date);
+                            this.resetForm();
+                        }
                     }
                 ],
                 { cancelable: false }
@@ -49,7 +54,30 @@ class Reservation extends Component {
                 date: '',
                 showModal: false
             });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.stuat !== 'granted') {
+                Alert.alert('Permission not granted to show notifications')
+            }
+            return permission;
         }
+        return permission;
+    }
+    
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            })
+        }
+    }
+
         
 
     render() {
